55fe4c325fb9972d821b588d0c06122963d52d8f
Manuel <manuel.mueller@audibene.de>
2018-04-26 18:19:33 +0200

    changing to latest version

--- convert.sh
+++ convert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ $1 == *.wav ]];
+if [ -a $1 == *.wav ];
 then
-  ffmpeg -i /home/input/$1 -y -c:a pcm_alaw /home/output/output.wav;
-  ffmpeg -i /home/output/output.wav -y -ab 128000 /home/output/$1
+  ffmpeg -i /home/fileconversion/$1 -y -c:a pcm_alaw /tmp/output.wav;
+  ffmpeg -i /tmp/output.wav -y -ab 128000 /home/output/$1
 fi
